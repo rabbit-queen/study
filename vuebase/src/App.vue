c3b181265a9ecc0eaa4443661aba8183687ba532
xhk <1916883971@qq.com>
2021-12-10 15:02:39 +0800

    vuebase

---
+++ vuebase/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="(item, ind) in arr" :key="ind" style="display: inline-block; margin-right: 10px">
+      <input type="checkbox" v-model="checkNumArr" :value="item" />
+      <span>{{ item }}</span>
+    </div>
+    <p>你选中的元素, 累加的值和为: {{ theSum }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      checkNumArr: [], //  保存用户选中的数字
+    }
+  },
+  computed: {
+    theSum(){
+      return this.checkNumArr.reduce((sum, arr) => {
+        return sum += arr
+      }, 0)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
